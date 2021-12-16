96939d4aaaf15c3443e344009e3b4a28a4ed9334
pozmanadam <52240390+pozmanadam@users.noreply.github.com>
2021-12-16 01:32:46 +0100

    Update evaluate_test_en.py

--- src/evaluate_test_en.py
+++ src/evaluate_test_en.py
@@ -12,8 +12,8 @@ import matplotlib.pyplot as plt
 get_ipython().run_line_magic('matplotlib', 'inline')
 import pickle
 import numpy as np
-pip install ipywidgets
-jupyter nbextension enable --py widgetsnbextension
+#pip install ipywidgets
+#jupyter nbextension enable --py widgetsnbextension
 
 # In[2]:
 
